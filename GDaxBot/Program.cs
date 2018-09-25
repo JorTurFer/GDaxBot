a94f56e56085563a8efb14246e86efbeed52462a
Jorge Turrado Ferrero <jorge_turrado@hotmail.es>
2018-09-25 21:09:39 +0200

    Añado la clase settings y los paquetes para DI

--- GDaxBot/Program.cs
+++ GDaxBot/Program.cs
@@ -1,12 +1,24 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.InlineQueryResults;
+using Telegram.Bot.Types.ReplyMarkups;
 
 namespace GDaxBot
 {
     class Program
     {
+        public static IConfigurationRoot Configuration { get; set; }
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
---
+++ GDaxBot/Settings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GDaxBot
+{
+    public class Settings
+    {
+        public string TelegramBotKey { get; set; }
+    }
+}
